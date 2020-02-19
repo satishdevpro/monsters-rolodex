0134494ee9a3f36898f067cdb3558445ec0ab5a3
your <uipro.satish@gmail.com>
2020-02-19 07:30:58 -0500

    Updates

---
+++ precache-manifest.44b0458b5a3bf8f060fa5d5a595df12c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f680b5adc32d444778a6687657931c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c3315d38c8f23b266f34",
+    "url": "/monsters-rolodex/static/css/main.277041cb.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3315d38c8f23b266f34",
+    "url": "/monsters-rolodex/static/js/main.c6a9af7b.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
